0575e75feecdb824ec657b56e00280b70f08a9dd
Jnanashish <jhandique1999@gmail.com>
2026-04-03 23:21:06 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React,{ useState } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Navigate } from "react-router-dom";
 
 import { UserContext } from "./Context/userContext";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
 
 import "./App.css";
 
@@ -19,15 +19,15 @@ import CompanyList from "./pages/CompanyList";
 import { getCookie } from "./Helpers/cookieHelpers";
 
 function App() {
-    // set user details in context
+    // set user details in context test
     // const [user, setUser] = useState(null);
-    const [user, setUser] = useState({ email: "jhandique1999@gmail.com" });
-    const [isAdmin, setIsAdmin] = useState(false);
+    const [user,setUser] = useState({ email: "jhandique1999@gmail.com" });
+    const [isAdmin,setIsAdmin] = useState(false);
 
     const isUserLogedin = !!user?.email || getCookie("isLogedIn"); // || "jhandique1999@gmail.com"
 
     // RouterWrapper is a function that returns a Route component
-    const RouterWrapper = (path, component, isSecured = true) => {
+    const RouterWrapper = (path,component,isSecured = true) => {
         return (
             <Route exact path={path} element={isSecured ? (isUserLogedin ? component : <Navigate to="/signin" />) : component} />
         );
@@ -35,18 +35,18 @@ function App() {
 
     return (
         <BrowserRouter>
-            <UserContext.Provider value={{ user, setUser, isAdmin, setIsAdmin }}>
+            <UserContext.Provider value={{ user,setUser,isAdmin,setIsAdmin }}>
                 <div className="App">
                     <Header />
                     <ToastContainer autoClose={2000} />
                     <Routes>
-                        {RouterWrapper("/signin", <Signin />, false)}
-                        {RouterWrapper("/addjob", <Addjobs />)}
-                        {RouterWrapper("/canvas", <Banners />)}
-                        {RouterWrapper("/addcompany", <AddCompanyDetails />)}
-                        {RouterWrapper("/companys", <CompanyList />)}
-                        {RouterWrapper("/jobs", <JobList />)}
-                        {RouterWrapper("/", <Signin />, false)}
+                        {RouterWrapper("/signin",<Signin />,false)}
+                        {RouterWrapper("/addjob",<Addjobs />)}
+                        {RouterWrapper("/canvas",<Banners />)}
+                        {RouterWrapper("/addcompany",<AddCompanyDetails />)}
+                        {RouterWrapper("/companys",<CompanyList />)}
+                        {RouterWrapper("/jobs",<JobList />)}
+                        {RouterWrapper("/",<Signin />,false)}
                     </Routes>
                 </div>
             </UserContext.Provider>
